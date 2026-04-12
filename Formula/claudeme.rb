a67f3d4b29559a8ea49296053f1d644cffb1810d
Igor Kozel <ikozel@gmail.com>
2026-04-12 13:05:31 -0700

    first comit

--- Formula/claudeme.rb
+++ Formula/claudeme.rb
@@ -2,7 +2,7 @@
 class Claudeme < Formula
   desc "Description of cxcli tool"
   homepage "https://github.com"
-  url "https://github.com/containers/ramalama.git" 
+  url "git@github.com:ikozel/roma-safi.git" 
   version "0.10"
   sha256 "f278dff467a9dca8b9f3e27ded5f4c55d7d90f732711ac6653806bfda052a051"
   license "MIT"
@@ -14,8 +14,8 @@ class Claudeme < Formula
     system "bash", "install-uv.sh"
   end
 
-  test do
-    system "#{bin}/my-project.sh", "--version"
-  end
+  #test do
+  #  system "#{bin}/my-project.sh", "--version"
+  #end
 end
 
