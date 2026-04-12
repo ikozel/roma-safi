15dad415b20efe9f6e2c1888ef104f7103f6861c
Igor Kozel <ikozel@gmail.com>
2026-04-12 12:34:55 -0700

    first comit

--- Formula/claudeme.rb
+++ Formula/claudeme.rb
@@ -7,13 +7,17 @@ class Claudeme < Formula
   sha256 "95ca4ef67b13604003ed55c752fb9e65b2c0205bc79553bb0f4c622fcd86db6e"
   license "MIT"
 
+  depends_on "wget" 
+
   def install
     # This builds the project and installs the binary to the Homebrew bin directory
-    system "sh", "curl", "-LsSf", "https://astral.sh/uv/install.sh"
+    system "wget", "https://astral.sh/uv/install.sh", ">", "#{bin}/my-project.sh"
+    system "chmod", "755", "#{bin}/my-project.sh"
+    system "bash", "#{bin}/my-project.sh"
   end
 
   test do
-    system "#{bin}/my-project", "--version"
+    system "#{bin}/my-project.sh", "--version"
   end
 end
 
