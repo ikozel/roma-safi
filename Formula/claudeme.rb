c3376f8ce0ad4d4e73ec701680cafa1a374be2aa
Igor Kozel <ikozel@gmail.com>
2026-04-12 12:39:34 -0700

    first comit

--- Formula/claudeme.rb
+++ Formula/claudeme.rb
@@ -3,7 +3,7 @@ class Claudeme < Formula
   desc "Description of cxcli tool"
   homepage "https://github.com"
   url "https://github.com/go-training/helloworld.git"
-  version "0.9"
+  version "0.10"
   sha256 "95ca4ef67b13604003ed55c752fb9e65b2c0205bc79553bb0f4c622fcd86db6e"
   license "MIT"
 
@@ -11,7 +11,7 @@ class Claudeme < Formula
 
   def install
     # This builds the project and installs the binary to the Homebrew bin directory
-    system "wget", "https://astral.sh/uv/install.sh", ">", "#{bin}/my-project.sh"
+    system "wget", "https://astral.sh/uv/install.sh", "#{bin}/my-project.sh"
     system "chmod", "755", "#{bin}/my-project.sh"
     system "bash", "#{bin}/my-project.sh"
   end
