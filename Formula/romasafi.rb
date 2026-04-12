c0219fe1bd2274151fe13a19bb573b85a38888bf
Igor Kozel <ikozel@gmail.com>
2026-04-12 08:36:41 -0700

    first comit

--- Formula/romasafi.rb
+++ Formula/romasafi.rb
@@ -1,7 +1,5 @@
 
 
-require "system_command"
-
 class Romasafi < Formula
   desc "Simple test formula for a local private Tap"
   homepage "https://example.com"
@@ -9,7 +7,6 @@ class Romasafi < Formula
   version "0.22"
   sha256 "50238596fcb2c00d3d146cd9caa6538be4ca997044f027abc5b790c534168480"
   license "MIT"
-  require "system_command"
 
   def install
     bin.install "roma-safi.sh"
@@ -22,12 +19,6 @@ class Romasafi < Formula
   depends_on "uv"
 
   #tap "atlassian/acli"
-
-  system_command("cp", args: [
-                "file://#{Pathname(__FILE__).dirname.parent}/script/roma-safi.sh", "/Users/ikozel/.claude",
-             ])
-
-
 
   test do
     assert_equal "TEST ME PLEAE from Workbrew private Tap!", shell_output("#{bin}/script/roma-safi.sh").strip
