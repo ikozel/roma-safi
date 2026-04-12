0bddf6f71b20e4322918ac963418596026e9cceb
Igor Kozel <ikozel@gmail.com>
2026-04-12 13:10:32 -0700

    first comit

--- Formula/claudeme.rb
+++ Formula/claudeme.rb
@@ -1,10 +1,10 @@
 
 class Claudeme < Formula
-  desc "Description of cxcli tool"
+  desc "Description of cxcli tool "
   homepage "https://github.com"
   url "https://github.com/ikozel/roma-safi.git" 
   version "0.10"
-  sha256 "c73ff7e5ae46accc57e108205801afc3d517da8c5fea6505979b3aa83afc9281"
+  sha256 "c0d6dd7a3cb13f92de91b5f07797259f33fb3f631087a67cad24b9a29e9dcca8"
   license "MIT"
 
   depends_on "wget" 
