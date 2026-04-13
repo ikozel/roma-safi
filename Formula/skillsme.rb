fbf7b07d89189234596b9cf8e730a8ebb7f33ef8
Igor Kozel <ikozel@gmail.com>
2026-04-12 17:16:36 -0700

    first comit

--- Formula/skillsme.rb
+++ Formula/skillsme.rb
@@ -2,8 +2,8 @@
 class Skillsme < Formula
   desc "Description of skills uploade"
   homepage "https://github.com"
-  url "..."
-  version "0.9"
+  url "https://github.com/go-training/helloworld.git"
+  version "0.12"
   sha256 "435cd17d939fab9028dbc16e79f2d5688cf085c86eedf07e4d55ebafca3ac703"
   license "MIT"
 
