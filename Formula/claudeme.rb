027c815ed8167372d80a3d68b47f2e4de4b80641
Igor Kozel <ikozel@gmail.com>
2026-04-13 07:58:25 -0700

    first comit

--- Formula/claudeme.rb
+++ Formula/claudeme.rb
@@ -3,11 +3,11 @@ class Claudeme < Formula
   desc "Description of cxcli tool "
   homepage "https://github.com"
   url "https://github.com/ikozel/roma-safi/blob/main/script/install-uv.sh"
-  version "0.13"
-  sha256 "705f6dbb31f12a98f885efdce55a9348e0706d1482fdde94795b2a8eaf185d66"
+  version "0.14"
+  sha256 "ffa901d985b06beee99319b5e965f5b05c62b0b91cb6af8099731e9bc3fa2892"
   license "MIT"
 
-  depends_on "wget" 
+  #depends_on "wget" 
 
   def install
     bin.install "install-uv.sh" 
