b8227b1efcdd443149b0ae152d75f3cf45fe2b67
Igor Kozel <ikozel@gmail.com>
2026-04-13 07:48:31 -0700

    first comit

--- Formula/claudeme.rb
+++ Formula/claudeme.rb
@@ -3,7 +3,7 @@ class Claudeme < Formula
   desc "Description of cxcli tool "
   homepage "https://github.com"
   url "https://github.com/ikozel/roma-safi/blob/main/script/install-uv.sh"
-  version "0.12"
+  version "0.13"
   sha256 "705f6dbb31f12a98f885efdce55a9348e0706d1482fdde94795b2a8eaf185d66"
   license "MIT"
 
