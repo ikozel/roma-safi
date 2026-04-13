62ca24ab3cf55f0537b61a2f59d0d2ef863196d2
Igor Kozel <ikozel@gmail.com>
2026-04-12 17:13:18 -0700

    first comit

---
+++ Formula/pluginsme.rb
@@ -0,0 +1,24 @@
+
+class Pluginsme < Formula
+  desc "Description of Plugins"
+  homepage "https://github.com"
+  url "https://github.com/go-training/helloworld.git"
+  version "0.9"
+  sha256 "9c091f60e8e965ef54b7d4030c881749aeac2b1f8b1eb71cc3ddad931e5645f5"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  def install
+    # This builds the project and installs the binary to the Homebrew bin directory
+    system "go", "mod", "init", "my-project"
+    system "go", "mod", "tidy"
+    system "go", "build", *std_go_args(output: bin/"my-project")
+    system "#{bin}/my-project"
+  end
+
+  test do
+    system "#{bin}/my-project", "--version"
+  end
+end
+
---
+++ Formula/skillsme.rb
@@ -0,0 +1,19 @@
+
+class Skillsme < Formula
+  desc "Description of skills uploade"
+  homepage "https://github.com"
+  url "..."
+  version "0.9"
+  sha256 "435cd17d939fab9028dbc16e79f2d5688cf085c86eedf07e4d55ebafca3ac703"
+  license "MIT"
+
+  def install
+    # This builds the project and installs the binary to the Homebrew bin directory
+    system "cp", "-r", "file://#{Pathname(__FILE__).dirname.parent}/script/jira", "/Users/ikozel/.claude/skills/"
+  end
+
+  test do
+    system "#{bin}/my-project", "--version"
+  end
+end
+
