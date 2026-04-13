0c57760c81ae45e8ceefe4443fb76c053be9858e
Igor Kozel <ikozel@gmail.com>
2026-04-12 18:07:54 -0700

    first comit

---
+++ Formula/rustme.rb
@@ -0,0 +1,20 @@
+
+class Rustme < Formula
+  desc "Description of Rust tool"
+  homepage "https://github.com"
+  url "https://github.com/meta-rust/rust-hello-world.git"
+  version "0.9"
+  sha256 "16e63ed66be2365eac617c494c53121afbbdbf0a0910a1116c8888bfc6f297e3"
+  license "MIT"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+    system "#{bin}/my-rust-project"
+  end
+
+  #test do
+  #  assert_match "my-rust-tool 1.0.0", shell_output("#{bin}/my-rust-tool --version")
+  #end
+end
